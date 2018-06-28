8f2c9be8192720745d60ef5bdbce764d3e5e9e4b
Uriel Parienti <urielprb@gmail.com>
2018-06-28 20:01:27 +0300

    basic add new investor

--- src/app/investors/investor-list/investor-list.component.ts
+++ src/app/investors/investor-list/investor-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {InvestorsService} from '../investor/investors-service';
 import Investor from '../../models/investor.model';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-investorlist',
@@ -10,7 +11,7 @@ import Investor from '../../models/investor.model';
 export class InvestorListComponent implements OnInit {
 
   investorsList: Investor[];
-  constructor(private investorsService: InvestorsService) { }
+  constructor(private investorsService: InvestorsService , private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.investorsService.getInvestors().subscribe((results) => {
@@ -32,5 +33,7 @@ export class InvestorListComponent implements OnInit {
       }
     });
   }
-
+  onLoadProfile() {
+    this.router.navigate(['../addInvestor/'], { relativeTo: this.route });
+  }
 }
