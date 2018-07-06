61a0cd6ad0f2297da9c3e67c973f0ff5dacf17a5
Uriel Parienti <urielprb@gmail.com>
2018-07-06 19:08:14 +0300

    for m validation and form submitting

--- src/app/component/rating/rating.component.ts
+++ src/app/component/rating/rating.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-rating',
@@ -6,11 +6,18 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./rating.component.scss']
 })
 export class RatingComponent implements OnInit {
+
   @Input()
   currentRate = 0;
+
+  @Output()
+  ratingValue = new EventEmitter<number>();
   constructor() { }
 
   ngOnInit() {
   }
 
+  testBinding() {
+    this.ratingValue.emit(this.currentRate);
+  }
 }
