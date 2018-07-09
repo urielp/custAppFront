544287870a41e952809a4a17fe85dc48462aedd0
urielp <urielprb@gmail.com>
2018-07-09 15:48:54 +0300

    adding projects list and single project component(hard coded for now)

--- src/app/projects/projects-routing.ts
+++ src/app/projects/projects-routing.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ProjectComponent} from './project/project.component';
 import {ProjectsComponent} from './projects.component';
+import {ProjectsListComponent} from './projects-list/projects-list.component';
 
 
 
@@ -10,7 +11,8 @@ const routes: Routes = [
   {
     path: '', component: ProjectsComponent,
     children: [
-      {path: 'project', component: ProjectComponent}
+      {path: 'project', component: ProjectComponent},
+      {path: 'projectList', component: ProjectsListComponent}
     ]
   }
 ];
